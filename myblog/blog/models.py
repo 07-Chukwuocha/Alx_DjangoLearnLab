b20a22214cdf6697bca62e61dcd86a421f2d3822
07-Chukwuocha <a0961150210@gmail.com>
2025-12-14 14:36:45 -0800

    Templates

---
+++ myblog/blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.title

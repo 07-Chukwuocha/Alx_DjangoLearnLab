1263cf84729c6a7be84ca9aeaec2eccee99c4cc3
07-Chukwuocha <a0961150210@gmail.com>
2026-01-02 15:32:37 -0800

    correction

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
 
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
+from .models import Post  # if inside same file, this line is not needed; otherwise make sure Post is defined above
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)  # MUST be exactly this name
 
     def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
+        return f'Comment by {self.author.username} on {self.post.title}'
 

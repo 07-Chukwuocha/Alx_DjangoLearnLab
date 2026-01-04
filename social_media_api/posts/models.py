8553b612c404c411759bf95185f2dc7472f3852f
07-chukwuocha <a0961150210@gmail.com>
2026-01-04 09:31:35 -0800

    post likes

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -20,3 +20,14 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} likes {self.post.title}"
+
+class Meta:
+        unique_together = ('post', 'user')     

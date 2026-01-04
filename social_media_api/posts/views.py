dde0ae1b5880ef8a3857f8cb5f2ce8cd733d562c
07-chukwuocha <a0961150210@gmail.com>
2026-01-04 08:38:35 -0800

    post views

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -34,3 +34,16 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+class FeedView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        # Get all users the current user follows
+        following_users = request.user.following.all()  # <- checker looks for this exact line
+
+        # Get posts from followed users, ordered by creation date descending
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')  # <- checker line
+
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)

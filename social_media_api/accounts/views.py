fcbe12d39bac6a5a3f203ff5a83a621999aa3096
07-chukwuocha <a0961150210@gmail.com>
2026-01-04 04:25:53 -0800

    new

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import RegisterSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        user = User.objects.get(username=response.data['username'])
+        token, _ = Token.objects.get_or_create(user=user)
+        response.data['token'] = token.key
+        return response
+
+
+class LoginView(APIView):
+    def post(self, request):
+        user = authenticate(
+            username=request.data.get('username'),
+            password=request.data.get('password')
+        )
+        if not user:
+            return Response({'error': 'Invalid credentials'}, status=400)
+
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+
+
+class ProfileView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            'username': user.username,
+            'email': user.email,
+            'bio': user.bio
+        })
